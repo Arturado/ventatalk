a1c0b54c53f7489d258dd816048b79030602bd6b
Arturo Vasquez <arturados@gmail.com>
2026-04-22 08:01:08 -0400

    fix: frontedn produccion standlone, eliminar dev mode en Docker

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
+ output: 'standalone', 
+ env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || "",
   },
